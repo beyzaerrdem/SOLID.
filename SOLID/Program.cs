38149405f81bc2b3a96ae138f7662eca29586863
beyzaerrdem <beyzardm502@gmail.com>
2023-10-05 13:48:19 +0300

    LSP

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,15 +1,21 @@
 ﻿
+using SOLID.LiskovSubstitution.LSPGood;
 using SOLID.OpenClosed.OCPGood2;
 
 //good way
-SalaryCalculator salaryCalculator = new SalaryCalculator();
-
+//SalaryCalculator salaryCalculator = new SalaryCalculator();
 //Console.WriteLine(salaryCalculator.SalaryCalculate(10000, new LowSalaryCalculate()));
 
 //good2 delegate
-Console.WriteLine(salaryCalculator.SalaryCalculate(10000, new LowSalaryCalculate().Calculate));
+//Console.WriteLine(salaryCalculator.SalaryCalculate(10000, new LowSalaryCalculate().Calculate));
+//Console.WriteLine(salaryCalculator.SalaryCalculate(10000, x =>
+//{
+//    return x * 10; //lambda ile metodu yazmadan bu şekilde
+//}));
 
-Console.WriteLine(salaryCalculator.SalaryCalculate(10000, x =>
-{
-    return x * 10; //lambda ile metodu yazmadan bu şekilde
-}));
+BasePhone basePhone = new IPhone();
+basePhone.Call();
+((ITakePhoto)basePhone).TakePhoto();
+
+basePhone = new Nokia();
+basePhone.Call();
