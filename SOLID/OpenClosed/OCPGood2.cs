ac8e65bb215fea32e5533eadae9f0d7675d066af
beyzaerrdem <beyzardm502@gmail.com>
2023-10-05 13:05:01 +0300

    OCPGood2 Delegate

---
+++ SOLID/OpenClosed/OCPGood2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.OpenClosed.OCPGood2
+{
+   
+    public class LowSalaryCalculate 
+    {
+        public decimal Calculate(decimal salary)
+        {
+            return salary * 2;
+        }
+    }
+
+    public class MiddleSalaryCalculate 
+    {
+        public decimal Calculate(decimal salary)
+        {
+            return salary * 3;
+        }
+    }
+
+    public class HighSalaryCalculate 
+    { 
+
+        public decimal Calculate(decimal salary)
+        {
+            return salary * 4;
+        }
+    }
+
+    public class ManagerSalaryCalculate 
+    {
+        public decimal Calculate(decimal salary)
+        {
+            return salary * 5;
+        }
+    }
+
+    public class SalaryCalculator
+    {
+        public decimal SalaryCalculate(decimal salary, Func<decimal, decimal> salaryCalculateDelegate) //delegate
+        {
+            return salaryCalculateDelegate(salary);
+        }
+    } 
+}
--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,7 +1,15 @@
 ﻿
-using SOLID.OpenClosed.OCPGood;
+using SOLID.OpenClosed.OCPGood2;
 
 //good way
 SalaryCalculator salaryCalculator = new SalaryCalculator();
 
-Console.WriteLine(salaryCalculator.SalaryCalculate(10000, new LowSalaryCalculate()));
+//Console.WriteLine(salaryCalculator.SalaryCalculate(10000, new LowSalaryCalculate()));
+
+//good2 delegate
+Console.WriteLine(salaryCalculator.SalaryCalculate(10000, new LowSalaryCalculate().Calculate));
+
+Console.WriteLine(salaryCalculator.SalaryCalculate(10000, x =>
+{
+    return x * 10; //lambda ile metodu yazmadan bu şekilde
+}));
